4ecf0b7fb9b323d7c42663bdc9b0d05402c355e6
Suredun254 <suremabeya@gmail.com>
2023-02-17 07:32:00 -0800

    prints whether negative, zero,or positive

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
+/*
  * main - entry point
  * Description: prints  if a number is positive or negative.
  * Return: 0
-/* betty style doc for function main goes there */
+ */
 int main(void)
 {
 	int n;
